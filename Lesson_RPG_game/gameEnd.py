fda923bc9109501920460fc5739ff987937526c0
Sergey Shulnyaev <charrlik@gmail.com>
2019-09-08 10:31:28 +0300

    init commit

--- Lesson_RPG_game/gameEnd.py
+++ Lesson_RPG_game/gameEnd.py
@@ -15,7 +15,7 @@ class hero():
         self.damage = damage
         self.inventory = inventory
         self.coord = coord
-    def pick_item(self, item): # item = (1,2,3) 1-название, 2 - повышение скорости, 3 - повышение урона
+    def pickItem(self, item): # item = (1,2,3) 1-название, 2 - повышение скорости, 3 - повышение урона
         self.inventory.append(item)
         
         self.speed += item[1]
@@ -31,9 +31,9 @@ class hero():
         elif direction == 'right':
             self.coord[0] -= self.speed 
     def shoot(self, enemy):
-        hero_dict[enemy].take_damage(self.damage)
+        heroDict[enemy].takeDamage(self.damage)
         
-    def take_damage(self, damage):
+    def takeDamage(self, damage):
         self.health -= damage
 
         
@@ -41,46 +41,46 @@ class field():
     def __init__(self, size=(40,40)):
         self.artifacts = {(-5,-5):('MagicBullet',10,10),(5,5):('CannonOfWisdom',20,20)}
         self.size = size
-    def picking_item(self, coord):
+    def pickingItem(self, coord):
         self.artifacts.pop(coord)
         
 h1 = hero('Gendalf',[5,5])
 h2 = hero('Smaug',[-5,-5])
-hero_dict= {'Gendalf' : h1, 'Smaug' : h2}
-game_field = field()
+heroDict= {'Gendalf' : h1, 'Smaug' : h2}
+gameField = field()
 
 
 gameend = 0
-while 1:
-    if gameend == 0 or hero_dict == {}:
-        for her in list(hero_dict.keys()):        
-            print("Ты {0} и находишься в {1}.".format(her,hero_dict[her].coord))
-            if game_field.artifacts.get(tuple(hero_dict[her].coord)):
-                print("Рядом лежит {0}".format(game_field.artifacts[tuple(hero_dict[her].coord)]))
+while True:
+    if gameend == 0 and heroDict != {}:
+        for her in list(heroDict.keys()):        
+            print("Ты {0} и находишься в {1}.".format(her,heroDict[her].coord))
+            if gameField.artifacts.get(tuple(heroDict[her].coord)):
+                print("Рядом лежит {0}".format(gameField.artifacts[tuple(heroDict[her].coord)]))
             print("Что ты хочешь сделать?(shoot, move, pick)")
             action = input()
             if action == 'move':
                 print("Куда ты хочешь двигаться?(up, down, right, left)")
-                hero_dict[her].move(input())
-                if abs(hero_dict[her].coord[0]) > game_field.size[0] / 2 or abs(hero_dict[her].coord[1]) > game_field.size[1] / 2:
-                    hero_dict[her].health = 0
-                    print('Герой {0} упал'.format(hero_dict[her].name))
-                    hero_dict.pop(her)
+                heroDict[her].move(input())
+                if abs(heroDict[her].coord[0]) > gameField.size[0] / 2 or abs(heroDict[her].coord[1]) > gameField.size[1] / 2:
+                    heroDict[her].health = 0
+                    print('Герой {0} упал'.format(heroDict[her].name))
+                    heroDict.pop(her)
             elif action == "shoot":
-                print("В кого ты хочешь выстрелить{0}".format(list(hero_dict.keys())))
+                print("В кого ты хочешь выстрелить{0}".format(list(heroDict.keys())))
                 enemy = input()
-                hero_dict[her].shoot(enemy)
-                if hero_dict[enemy].health <= 0:
+                heroDict[her].shoot(enemy)
+                if heroDict[enemy].health <= 0:
                     gameend = 1
                     print('Герой {0}  умер'.format(enemy))
-                    hero_dict.pop(enemy)
+                    heroDict.pop(enemy)
                 else:
-                    print('Ты нанёс {0} урона игроку {1}, у него осталось {2} здоровья'.format(hero_dict[her].damage, enemy, hero_dict[enemy].health)) 
+                    print('Ты нанёс {0} урона игроку {1}, у него осталось {2} здоровья'.format(heroDict[her].damage, enemy, heroDict[enemy].health)) 
             elif action == 'pick':
-                item = game_field.artifacts.get(tuple(hero_dict[her].coord))
+                item = gameField.artifacts.get(tuple(heroDict[her].coord))
                 if item != None:
-                    game_field.picking_item(tuple(hero_dict[her].coord))
-                    hero_dict[her].pick_item(item)
+                    gameField.pickingItem(tuple(heroDict[her].coord))
+                    heroDict[her].pickItem(item)
                     print("Вы подобрали {0}".format(item[0]))
             elif action == 'exit':
                 gameend = 1
