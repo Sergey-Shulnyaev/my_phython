206e2fd7fcff0cd876d9f99d4274c6cc253cf951
Sergey-Shulnyaev <charrlik@gmail.com>
2019-09-10 12:44:49 +0300

    Add files via upload

---
+++ AVTOCEH.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 11:50:12 2019
+
+@author: mag
+"""
+import random
+
+class Сustomer():
+    def __init__(self, name, address, phoneNumber, money, car):
+        self.__name = name
+        self.__address = address
+        self.__phoneNumber = phoneNumber
+        self.__money = money
+        self.__car = car
+
+        
+        
+
+    
+class Сar():
+    def __init__(self, brand, model, year, breakage):
+        
+        self.__brand = brand
+        self.__model = model
+        self.__year = year
+        self.__breakage = breakage
+        
+#class Сompany():
+#    def __init__(self, money): 
+    
